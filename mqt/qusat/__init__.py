6f8cec1af6604e3df4ef9e94ecc975e39ce6e23b
lucas <lucberent@gmail.com>
2022-03-07 12:17:35 +0100

    fix init file

--- mqt/qusat/__init__.py
+++ mqt/qusat/__init__.py
@@ -2,3 +2,4 @@
 # This file is part of MQT QuSAT library which is released under the MIT license.
 # See file README.md or go to https://github.com/lucasberent/qsatencoder for more information.
 #
+from .pyqusat import checkEquivalent
