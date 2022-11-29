fc0fdea05025d4755055a84e91328791b92e79df
Lukas Burgholzer <lukas.burgholzer@jku.at>
2022-11-29 13:23:01 +0100

    🔧 switch to PEP 621 for python package configuration
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- setup.py
+++ setup.py
@@ -2,37 +2,36 @@ import os
 import re
 import subprocess
 import sys
+from contextlib import suppress
+from pathlib import Path
 
-from setuptools import setup, Extension, find_namespace_packages
+from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir='', namespace=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
-        self.namespace = namespace
+    def __init__(self, name: str, sourcedir: str = "") -> None:
+        super().__init__(name, sources=[])
+        self.sourcedir = str(Path(sourcedir).resolve())
 
 
 class CMakeBuild(build_ext):
-    def build_extension(self, ext):
-        from setuptools_scm import get_version
-        version = get_version(root='.', relative_to=__file__)
+    def build_extension(self, ext: CMakeExtension) -> None:
+        from setuptools_scm import get_version  # type: ignore[import]
 
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.namespace + ext.name)))
-        # required for auto-detection of auxiliary "native" libs
-        if not extdir.endswith(os.path.sep):
-            extdir += os.path.sep
+        version = get_version(root=".", relative_to=__file__)
+
+        extdir = str(Path(self.get_ext_fullpath(ext.name)).parent.resolve())  # type: ignore[no-untyped-call]
 
         cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
-        cfg = 'Debug' if self.debug else 'Release'
+        cfg = "Debug" if self.debug else "Release"
 
         cmake_args = [
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
-            "-DPYTHON_EXECUTABLE={}".format(sys.executable),
-            "-DQUSAT_VERSION_INFO={}".format(version),
-            "-DCMAKE_BUILD_TYPE={}".format(cfg),
-            "-DBINDINGS=ON"
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
+            f"-DPYTHON_EXECUTABLE={sys.executable}",
+            f"-DQUSAT_VERSION_INFO={version}",
+            f"-DCMAKE_BUILD_TYPE={cfg}",
+            "-DBINDINGS=ON",
         ]
         build_args = []
 
@@ -58,70 +57,34 @@ class CMakeBuild(build_ext):
                 cmake_args += ["-A", plat_to_cmake[self.plat_name]]
             # Multi-config generators have a different way to specify configs
             if not single_config:
-                cmake_args += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)]
+                cmake_args += [f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"]
                 build_args += ["--config", cfg]
 
-            # cross-compile support for macOS - respect ARCHFLAGS if set
+        # cross-compile support for macOS - respect ARCHFLAGS if set
         if sys.platform.startswith("darwin"):
             archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
             if archs:
                 cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
 
-            # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level across all generators.
-        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
-            if hasattr(self, "parallel") and self.parallel:
-                build_args += ["-j{}".format(self.parallel)]
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level across all generators.
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ and hasattr(self, "parallel") and self.parallel:
+            build_args += [f"-j{self.parallel}"]
 
         if sys.platform == "win32":
-            cmake_args += ['-T', 'ClangCl']
+            cmake_args += ["-T", "ClangCl"]
 
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        else:
-            os.remove(self.build_temp + "/CMakeCache.txt")
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
-        subprocess.check_call(['cmake', '--build', '.', '--target', ext.name] + build_args, cwd=self.build_temp)
+        build_dir = Path(self.build_temp)
+        build_dir.mkdir(parents=True, exist_ok=True)
+        with suppress(FileNotFoundError):
+            Path(build_dir / "CMakeCache.txt").unlink()
 
-
-README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                           'README.md')
-with open(README_PATH) as readme_file:
-    README = readme_file.read()
+        subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp)
+        subprocess.check_call(
+            ["cmake", "--build", ".", "--target", ext.name.split(".")[-1]] + build_args,
+            cwd=self.build_temp,
+            )
 
 setup(
-    name='mqt.qusat',
-    author='Lucas Berent',
-    author_email='lucas.berent@tum.de',
-    description='MQT QuSAT - A Tool for Utilizing SAT in Quantum Computing',
-    long_description=README,
-    long_description_content_type="text/markdown",
-    python_requires='>=3.6',
-    license="MIT",
-    url="https://github.com/cda-tum/qusat",
-    ext_modules=[CMakeExtension('pyqusat', namespace='mqt.qusat.')],
+    ext_modules=[CMakeExtension("mqt.qusat.pyqusat")],
     cmdclass={"build_ext": CMakeBuild},
-    zip_safe=False,
-    packages=find_namespace_packages(include=['mqt.*']),
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        "Programming Language :: C++",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: MacOS",
-        "Operating System :: POSIX :: Linux",
-        "Intended Audience :: Science/Research",
-        "Natural Language :: English",
-        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
-    ],
-    keywords="MQT quantum satisfiability",
-    project_urls={
-        'Source': 'https://github.com/cda-tum/qusat',
-        'Tracker': 'https://github.com/cda-tum/qusat/issues'
-    }
 )
