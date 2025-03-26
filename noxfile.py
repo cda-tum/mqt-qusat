d8926fdb2d1399bcb20c2c896533044b385d2094
burgholzer <burgholzer@me.com>
2025-03-26 10:10:20 +0100

    ⬆️ update noxfile

--- noxfile.py
+++ noxfile.py
@@ -3,12 +3,16 @@
 from __future__ import annotations
 
 import os
+import shutil
 
 import nox
 
+nox.needs_version = ">=2024.3.2"
+nox.options.default_venv_backend = "uv"
+
 nox.options.sessions = ["lint"]
 
-PYTHON_ALL_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
+PYTHON_ALL_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
 
 if os.environ.get("CI", None):
     nox.options.error_on_missing_interpreters = True
@@ -17,5 +21,7 @@ if os.environ.get("CI", None):
 @nox.session(reuse_venv=True)
 def lint(session: nox.Session) -> None:
     """Run the linter."""
-    session.install("pre-commit")
-    session.run("pre-commit", "run", "--all-files", *session.posargs)
+    if shutil.which("pre-commit") is None:
+        session.install("pre-commit")
+
+    session.run("pre-commit", "run", "--all-files", *session.posargs, external=True)
